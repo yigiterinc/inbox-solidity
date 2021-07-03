de5e760be0aee6ad6b08e9101926eee05916ad1b
Yigit Erinc <yigit.erinc@ozu.edu.tr>
2021-07-03 17:40:44 +0300

    Compile script added

---
+++ contracts/Inbox.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.26;
+
+contract inbox {
+    string public message;
+
+    function Inbox(string memory initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
